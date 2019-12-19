c60ec3145ff7344ed09655282ed44af7a80289b2
HassanAlMohandes <58953681+HassanAlMohandes@users.noreply.github.com>
2019-12-19 02:38:14 +0200

    web

---
+++ Django-master/myproject/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Hassan_Mohmed\Desktop\Django-master\myproject\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
